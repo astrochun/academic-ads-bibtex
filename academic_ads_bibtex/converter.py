c96034c591d64bd146cc8461aa66bec505156eac
Chun Ly <astro.chun@gmail.com>
2020-12-27 14:56:10 -0700

    Convert: add docstrings

--- academic_ads_bibtex/converter.py
+++ academic_ads_bibtex/converter.py
@@ -1,9 +1,30 @@
-from pathlib import Path
-
-
 class Convert:
     """
     Main class to perform BibTeX conversion for Academic compatibility
+
+    :param filename: str or pathlib.Path object
+    :param db_filename: str or pathlib.Path object
+    :param out_filename: str or pathlib.Path object
+
+    Attributes
+    ----------
+    bibtex_content: str (from import_file)
+    db_dict: dict of journal database (from import_database)
+    bibtex_revised: str (from replace)
+
+    Methods
+    -------
+    import_file()
+      Import BibTeX file
+
+    import_database()
+      Import journal database file
+
+    replace()
+      Replace journal abbreviations
+
+    write_file()
+      Write revised BibTeX file
     """
 
     def __init__(self, filename, db_filename, out_filename):
@@ -20,12 +41,14 @@ class Convert:
         self.bibtex_revised = self.replace()
 
     def import_file(self):
+        """Import BibTeX file"""
         with open(self.filename, 'r') as f:
             bibtex_content = f.read()
         f.close()
         return bibtex_content
 
     def import_database(self):
+        """Import journal database file"""
         with open(self.db_filename, 'r') as f:
             content = f.read()
         db_list = content.split("\n")
@@ -34,6 +57,7 @@ class Convert:
         return db_dict
 
     def replace(self):
+        """Replace journal abbreviations"""
         bibtex_revised = str(self.bibtex_content)
         for key, value in self.db_dict.items():
             bibtex_revised = bibtex_revised.replace(f'\{key}', value)
@@ -41,6 +65,7 @@ class Convert:
         return bibtex_revised
 
     def write_file(self):
+        """Write revised BibTeX file"""
         print(f"Writing : {self.out_filename}")
         with open(self.out_filename, 'w') as f:
             f.writelines(self.bibtex_revised)
