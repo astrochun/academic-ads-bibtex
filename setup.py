c209f6ac06b357c64a130732811c53b61d2e1179
Chun Ly <astro.chun@gmail.com>
2021-01-02 22:47:32 -0700

    Forgot to bump version to v0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='academic-ads-bibtex',
-    version='0.1.4',
+    version='0.1.5',
     packages=['academic_ads_bibtex'],
     scripts=['bin/academic_ads_bibtex'],
     url='https://github.com/astrochun/academic-ads-bibtex',
