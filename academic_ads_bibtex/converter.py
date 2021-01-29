7aef0fb9377335b6719934e013633bcece1ed46d
Chun Ly <astro.chun@gmail.com>
2021-01-29 16:23:55 -0700

    Possible fix: Upgrade sphinx for minimum version

--- academic_ads_bibtex/converter.py
+++ academic_ads_bibtex/converter.py
@@ -1,3 +1,6 @@
+from typing import Union
+from pathlib import Path
+
 from .logger import log_stdout
 import pkg_resources
 
@@ -6,31 +9,19 @@ class Convert:
     """
     Main class to perform BibTeX conversion for Academic compatibility
 
-    :param filename: str or pathlib.Path object
-    :param db_filename: str or pathlib.Path object
-    :param out_filename: str or pathlib.Path object
+    :param filename: BibTeX file
+    :param db_filename: Database file
+    :param out_filename: Modified BibTeX file
     :param log: LogClass or logger object
 
-    :ivar bibtex_content: str (from import_file)
-    :ivar db_dict: dict of journal database (from import_database)
-    :ivar bibtex_revised: str (from replace)
-
-    Methods
-    -------
-    import_file()
-      Import BibTeX file
-
-    import_database()
-      Import journal database file
-
-    replace()
-      Replace journal abbreviations
-
-    write_file()
-      Write revised BibTeX file
+    :ivar bibtex_content: str (from ``import_file`` method)
+    :ivar db_dict: dict of journal database (from ``import_database`` method)
+    :ivar bibtex_revised: str (from ``replace`` method)
     """
 
-    def __init__(self, filename, db_filename, out_filename, log=None):
+    def __init__(self, filename: Union[str, Path],
+                 db_filename: Union[str, Path],
+                 out_filename: Union[str, Path], log=None):
 
         if log is None:
             log = log_stdout()
@@ -47,7 +38,7 @@ class Convert:
         # Update journal to full name
         self.bibtex_revised = self.replace()
 
-    def import_file(self):
+    def import_file(self) -> str:
         """Import BibTeX file"""
         self.log.info(f"Reading: {self.filename}")
         with open(self.filename, 'r') as f:
@@ -55,7 +46,7 @@ class Convert:
         f.close()
         return bibtex_content
 
-    def import_database(self):
+    def import_database(self) -> dict:
         """Import journal database file"""
         if isinstance(self.db_filename, str):
             self.log.info(f"Reading: {self.db_filename}")
@@ -76,7 +67,7 @@ class Convert:
 
         return db_dict
 
-    def replace(self):
+    def replace(self) -> str:
         """Replace journal abbreviations"""
         bibtex_revised = str(self.bibtex_content)
         for key, value in self.db_dict.items():
